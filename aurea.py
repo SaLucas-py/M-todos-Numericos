8855c53bb59f8771f27efc96a6394bc55329c267
Lucas <82826549+SaLucas-py@users.noreply.github.com>
2021-10-29 23:45:38 -0300

    Add files via upload

--- aurea.py
+++ aurea.py
@@ -1,3 +1,14 @@
+#Para que o programa possa ser executado precisamos instalar algumas bibliotecas
+
+#pip install sympy
+#pip install numpy
+
+#Exemplos de entrada:
+#Equação: x ** 2 - 6 * x + 15
+#intervalo a: 0
+#intervalo b: 10
+#iteracoes: 5
+
 import time
 import numpy as np
 from sympy import *
